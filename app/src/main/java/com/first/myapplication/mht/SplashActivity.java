8fbe0d7acd2098f58cf97766938454eaec843af3
Nikhil Singh <nikhil.singh@olx.com>
2020-03-17 17:14:38 +0530

    Changed app icon + Splash ui Changed

--- app/src/main/java/com/first/myapplication/mht/SplashActivity.java
+++ app/src/main/java/com/first/myapplication/mht/SplashActivity.java
@@ -2,8 +2,13 @@ package com.first.myapplication.mht;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,10 +16,16 @@ import com.google.firebase.auth.FirebaseUser;
 public class SplashActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    ConstraintLayout constraintLayoutSplash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        constraintLayoutSplash = findViewById(R.id.cl_splash);
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.slide_up);
+        constraintLayoutSplash.setAnimation(animation);
 
         mAuth= FirebaseAuth.getInstance();
         Handler handler = new Handler();
