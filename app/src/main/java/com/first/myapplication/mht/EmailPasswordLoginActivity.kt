9b54a30d24aaf61dcbfc6388f86770d996748842
Nikhil Singh <nikhil.singh@olx.com>
2020-03-18 16:25:01 +0530

    Email Password UI Started

---
+++ app/src/main/java/com/first/myapplication/mht/EmailPasswordLoginActivity.kt
@@ -0,0 +1,18 @@
+package com.first.myapplication.mht
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_email_password_login.*
+
+class EmailPasswordLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_password_login)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.slide_up)
+
+        cl_email_login.startAnimation(animation)
+
+    }
+}
