42919e2c25729007273d421302a3a42a81fd2c51
Nikhil Singh <nikhil.singh@olx.com>
2020-03-21 19:02:32 +0530

    Added prompt while signing out

--- app/src/main/java/com/first/myapplication/mht/themeActivity.java
+++ app/src/main/java/com/first/myapplication/mht/themeActivity.java
@@ -1,5 +1,6 @@
 package com.first.myapplication.mht;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,6 +9,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +21,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
@@ -108,12 +111,32 @@ public class themeActivity extends AppCompatActivity implements View.OnClickList
                     return true;
                 }
                 else if(menuItem.getItemId()==R.id.signout){
-                    FirebaseAuth.getInstance().signOut();
-                    Intent i = new Intent(themeActivity.this, MainActivity.class);
-                    i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(i);
-                    finish();
+
+
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(themeActivity.this);
+                    alertDialogBuilder.setTitle("Are you sure you want to Log out ?");
+                    alertDialogBuilder.setIcon(R.drawable.ic_error);
+                    alertDialogBuilder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            FirebaseAuth.getInstance().signOut();
+                            Intent i = new Intent(themeActivity.this, MainActivity.class);
+                            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(i);
+                            finish();
+                        }
+                    });
+                    alertDialogBuilder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    // create and show the alert dialog
+                    AlertDialog dialog = alertDialogBuilder.create();
+                    dialog.show();
                     return true;
+
                 }
                 return false;
             }
