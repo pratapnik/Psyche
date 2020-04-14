8be83b42ad15bd1c43b10abe9b82ff6210e3d195
Nikhil Singh <nikhil.singh@olx.com>
2020-04-15 00:28:16 +0530

    changed profile screen to only email screen

--- app/src/main/java/com/first/myapplication/mht/ProfileActivity.kt
+++ app/src/main/java/com/first/myapplication/mht/ProfileActivity.kt
@@ -2,19 +2,25 @@ package com.first.myapplication.mht
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile.*
 
 
 class ProfileActivity : AppCompatActivity() {
 
+    var profileDisplayDetails = "You are Logged in as"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
         val firebaseUser = FirebaseAuth.getInstance().currentUser
-        tvProfileEmail.text = firebaseUser?.email
-        tvProfileName.text = firebaseUser?.displayName
+
+        profileDisplayDetails = profileDisplayDetails.plus(" ").plus(firebaseUser?.email)
+
+        tvProfileEmail.text = profileDisplayDetails
+        tvProfileEmail.setTextColor(resources.getColor(R.color.textColor))
 
         toolbar_profile.setNavigationOnClickListener {
             onBackPressed()
