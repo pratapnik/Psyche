2f551e569bdca0a166d1fc9f9bfb8b3d0f337fae
Nikhil Singh <nikhil.singh@olx.com>
2020-04-16 02:23:30 +0530

    Added sound for meditation

--- app/src/main/java/com/first/myapplication/mht/MeditationFragment.kt
+++ app/src/main/java/com/first/myapplication/mht/MeditationFragment.kt
@@ -1,6 +1,7 @@
 package com.first.myapplication.mht
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v4.app.Fragment
@@ -19,6 +20,7 @@ class MeditationFragment : Fragment() {
 
     lateinit var tvTimer: TextView
     lateinit var startButton : Button
+    lateinit var mediaPlayer:MediaPlayer
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -27,6 +29,7 @@ class MeditationFragment : Fragment() {
 
         tvTimer = view.findViewById(R.id.tvCountDownTimer)
         startButton = view.findViewById(R.id.btnStartExercise)
+        mediaPlayer = MediaPlayer.create(activity, R.raw.meditationsound)
 
         return view
     }
@@ -34,6 +37,9 @@ class MeditationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         startButton.setOnClickListener {
+            mediaPlayer.isLooping = true
+            mediaPlayer.start()
+            mediaPlayer.setVolume(100F, 100F)
             startButton.isClickable = false
             startButton.alpha = 0.5F
             val timer = object : CountDownTimer(60000, 1000) {
@@ -48,10 +54,18 @@ class MeditationFragment : Fragment() {
                     startButton.text = "START AGAIN"
                     tvTimer.textSize = 24F
                     tvTimer.text = "One minute meditation is successfully completed"
+                    mediaPlayer.stop()
+                    mediaPlayer.release()
                 }
             }
             timer.start()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.stop()
+        mediaPlayer.release()
     }
 
 }
