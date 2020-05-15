cf446ac03c219f9bac9d4a37bb6b324e9ddecd4f
Nikhil Singh <nikhil.singh@olx.com>
2020-05-15 22:12:42 +0530

    Get about us pic from firebase

--- app/src/main/java/com/first/myapplication/mht/AboutUsActivity.kt
+++ app/src/main/java/com/first/myapplication/mht/AboutUsActivity.kt
@@ -4,7 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.firebase.client.DataSnapshot
+import com.firebase.client.Firebase
+import com.firebase.client.FirebaseError
+import com.firebase.client.ValueEventListener
+import com.first.myapplication.mht.utils.getProgressDrawable
 import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutUsActivity : AppCompatActivity() {
@@ -12,6 +19,20 @@ class AboutUsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val aboutUsImageUrl = "https://mental-health-tracker-bb023.firebaseio.com/images/about"
+        val firebase = Firebase(aboutUsImageUrl)
+
+        firebase.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val url = dataSnapshot.getValue(String::class.java)
+                Glide.with(applicationContext).load(url).centerCrop().placeholder(getProgressDrawable(iv_about.context)).into(iv_about);
+            }
+
+            override fun onCancelled(firebaseError: FirebaseError) {
+                Toast.makeText(applicationContext, "There is some internet error", Toast.LENGTH_LONG).show()
+            }
+        })
 
         //To go to developer's linkedin profilepage
         btnLinkedInProfile.setOnClickListener {
@@ -36,6 +57,8 @@ class AboutUsActivity : AppCompatActivity() {
         toolbar_about.setNavigationOnClickListener(View.OnClickListener {
             onBackPressed()
         })
+
+
     }
 }
 
