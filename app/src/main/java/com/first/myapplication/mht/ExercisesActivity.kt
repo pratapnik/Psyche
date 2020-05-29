04343c781603a379f97787e4d4f781afa9cf84c9
Nikhil Singh <nikhil.singh@olx.com>
2020-05-29 13:12:39 +0530

    Code moved to fragment

--- app/src/main/java/com/first/myapplication/mht/ExercisesActivity.kt
+++ app/src/main/java/com/first/myapplication/mht/ExercisesActivity.kt
@@ -1,34 +1,10 @@
 package com.first.myapplication.mht
 
-import android.app.ProgressDialog
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.first.myapplication.mht.data.BoredApiDataModel
-import com.first.myapplication.mht.data.BoredApiService
-import com.first.myapplication.mht.data.JarvisJokesFormatClass
-import com.first.myapplication.mht.fragments.activities.ActivitiesHomeFragment
-import com.first.myapplication.mht.utils.hideProgressDialogWithTitle
-import com.first.myapplication.mht.utils.showProgressDialogWithTitle
-import com.first.myapplication.mht.utils.showSnackBar
-import com.first.myapplication.mht.widgets.JarvisJokePopupDialog
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_exercises.*
-import kotlin.math.log
 
-class ExercisesActivity : AppCompatActivity(), JarvisJokePopupDialog.ActionListener{
-
-    lateinit var exerciseBundle: Bundle
-    lateinit var jokePopupDialog: JarvisJokePopupDialog
-    lateinit var progressDialog: ProgressDialog
-
-    private val namesAgeApiService = BoredApiService()
+class ExercisesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,75 +13,5 @@ class ExercisesActivity : AppCompatActivity(), JarvisJokePopupDialog.ActionListe
         ivExerciseBack.setOnClickListener {
             onBackPressed()
         }
-
-//        progressDialog = ProgressDialog(this)
-//        exerciseBundle = Bundle()
-//        jokePopupDialog = JarvisJokePopupDialog()
-//
-//        llMeditation.setOnClickListener {
-//            exerciseBundle.putString("exType", "Meditation")
-//            exerciseBundle.putString("fragmentType", "meditation")
-//            loadExcercise(exerciseBundle)
-//        }
-//
-//        btnRandomJoke.setOnClickListener {
-//            showProgressDialogWithTitle("Getting Joke", progressDialog)
-//            val result = namesAgeApiService.getActivity()
-//            result.subscribeOn(Schedulers.newThread())
-//                    .observeOn(AndroidSchedulers.mainThread())
-//                    .subscribeWith(object : DisposableSingleObserver<BoredApiDataModel>() {
-//                        override fun onSuccess(boredApiFromApi: BoredApiDataModel) {
-//                            val jokesFormatClass = JarvisJokesFormatClass(boredApiFromApi.jokeSetup, boredApiFromApi.jokePunchline)
-//                            openJokeDialog(jokesFormatClass)
-//                        }
-//
-//                        override fun onError(e: Throwable) {
-//                            clExercise.showSnackBar(getString(R.string.label_error_message))
-//                            e.printStackTrace()
-//                        }
-//                    })
-//
-//        }
-    }
-
-//    private fun loadExcercise(exerciseBundle: Bundle) {
-//        val displayExerciseIntent = Intent(this, DisplayExerciseActivity::class.java)
-//        displayExerciseIntent.putExtra("exTypeBundle", exerciseBundle)
-//        startActivity(displayExerciseIntent)
-//    }
-//
-//    private fun copyText(text: String) {
-//        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-//        val clip = ClipData.newPlainText("joke", text)
-//        clipboard.setPrimaryClip(clip)
-//        clExercise.showSnackBar("Joke is copied to clipboard")
-//    }
-//
-//    private fun sendJoke(text: String) {
-//        val shareIntent = Intent(Intent.ACTION_SEND)
-//        shareIntent.putExtra(Intent.EXTRA_TEXT, text)
-//        shareIntent.type = "text/plain"
-//        val sendIntent = Intent.createChooser(shareIntent, "Send Joke")
-//        startActivity(sendIntent)
-//    }
-//
-//    fun openJokeDialog(jokesFormatClass: JarvisJokesFormatClass) {
-//        val fragmentTransaction = supportFragmentManager.beginTransaction()
-//        fragmentTransaction.addToBackStack(null)
-//        jokePopupDialog.setJokes(jokesFormatClass)
-//        jokePopupDialog.show(
-//                fragmentTransaction,
-//                resources.getString(R.string.label_joke_popup_dialog)
-//        )
-//        jokePopupDialog.actionListener = this
-//        hideProgressDialogWithTitle(progressDialog)
-//    }
-
-    override fun onCopyListener(jokeText: String) {
-//        copyText(jokeText)
-    }
-
-    override fun onShareJokeListener(jokeText: String) {
-//        sendJoke(jokeText)
     }
 }
--- app/src/main/java/com/first/myapplication/mht/fragments/activities/ActivitiesHomeFragment.kt
+++ app/src/main/java/com/first/myapplication/mht/fragments/activities/ActivitiesHomeFragment.kt
@@ -1,5 +1,10 @@
 package com.first.myapplication.mht.fragments.activities
 
+import android.app.ProgressDialog
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,10 +12,23 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.first.myapplication.mht.R
+import com.first.myapplication.mht.data.BoredApiDataModel
+import com.first.myapplication.mht.data.BoredApiService
+import com.first.myapplication.mht.data.JarvisJokesFormatClass
+import com.first.myapplication.mht.utils.hideProgressDialogWithTitle
+import com.first.myapplication.mht.utils.showProgressDialogWithTitle
+import com.first.myapplication.mht.utils.showSnackBar
+import com.first.myapplication.mht.widgets.JarvisJokePopupDialog
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_activities_home.*
 
-class ActivitiesHomeFragment : Fragment() {
+class ActivitiesHomeFragment : Fragment(), JarvisJokePopupDialog.ActionListener {
 
+    lateinit var jokePopupDialog: JarvisJokePopupDialog
+    lateinit var progressDialog: ProgressDialog
+    private val namesAgeApiService = BoredApiService()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -23,6 +41,66 @@ class ActivitiesHomeFragment : Fragment() {
         llMeditation.setOnClickListener {
             view.findNavController().navigate(R.id.action_activitiesHomeFragment_to_meditationFragment)
         }
+
+        progressDialog = ProgressDialog(context)
+        jokePopupDialog = JarvisJokePopupDialog()
+
+
+        btnRandomJoke.setOnClickListener {
+            showProgressDialogWithTitle("Getting Joke", progressDialog)
+            val result = namesAgeApiService.getActivity()
+            result.subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeWith(object : DisposableSingleObserver<BoredApiDataModel>() {
+                        override fun onSuccess(boredApiFromApi: BoredApiDataModel) {
+                            val jokesFormatClass = JarvisJokesFormatClass(boredApiFromApi.jokeSetup, boredApiFromApi.jokePunchline)
+                            openJokeDialog(jokesFormatClass)
+                        }
+
+                        override fun onError(e: Throwable) {
+                            clExercise.showSnackBar(getString(R.string.label_error_message))
+                            e.printStackTrace()
+                        }
+                    })
+
+        }
+
+    }
+
+    private fun copyText(text: String) {
+        val clipboard = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("joke", text)
+        clipboard.setPrimaryClip(clip)
+        clExercise.showSnackBar("Joke is copied to clipboard")
+    }
+
+    private fun sendJoke(text: String) {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text)
+        shareIntent.type = "text/plain"
+        val sendIntent = Intent.createChooser(shareIntent, "Send Joke")
+        startActivity(sendIntent)
+    }
+
+    fun openJokeDialog(jokesFormatClass: JarvisJokesFormatClass) {
+        val fragmentTransaction = childFragmentManager.beginTransaction()
+        fragmentTransaction.addToBackStack(null)
+        jokePopupDialog.actionListener = this
+        jokePopupDialog.setJokes(jokesFormatClass)
+        jokePopupDialog.show(
+                fragmentTransaction,
+                resources.getString(R.string.label_joke_popup_dialog)
+        )
+        hideProgressDialogWithTitle(progressDialog)
+    }
+
+    override fun onCopyListener(jokeText: String) {
+        copyText(jokeText)
+    }
+
+    override fun onShareJokeListener(jokeText: String) {
+        sendJoke(jokeText)
     }
 
 }
+
